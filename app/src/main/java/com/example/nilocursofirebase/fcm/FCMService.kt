a617ee616e95d91b131ab36202551db9bb39274a
Orly <c_r_bro@hotmail.com>
2023-03-25 21:44:39 -0400

    Seccion 10 Firebase Cloud Messaging(FCM), Capitulo 135 Recibir notificaciones en primer plano

--- app/src/main/java/com/example/nilocursofirebase/fcm/FCMService.kt
+++ app/src/main/java/com/example/nilocursofirebase/fcm/FCMService.kt
@@ -1,19 +1,29 @@
 package com.example.nilocursofirebase.fcm
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.example.nilocursofirebase.Constants
+import com.example.nilocursofirebase.R
+import com.example.nilocursofirebase.product.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.RemoteMessage.Notification
 
-class FCMService:FirebaseMessagingService() {
+class FCMService : FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
 
         registerNewTokenLocal(newToken)
     }
 
-    private fun registerNewTokenLocal(newToken: String){
+    private fun registerNewTokenLocal(newToken: String) {
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
 
         preferences.edit {
@@ -22,5 +32,33 @@ class FCMService:FirebaseMessagingService() {
         }
 
         Log.i("new token", newToken)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        remoteMessage.notification?.let {
+            sendNotification(it)
+        }
+    }
+
+    private fun sendNotification(notification: Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = getString(R.string.notification_channel_id_default)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
